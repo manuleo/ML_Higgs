4f486b427f3b7135ac176d40d9d333ab763a5b3d
manuleo <manuel.leone97@gmail.com>
2019-10-11 13:59:09 +0200

    first

---
+++ Manuel/ex03/template/build_polynomial.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""implement a polynomial basis function."""
+
+import numpy as np
+
+
+def build_poly(x, degree):
+    """polynomial basis functions for input data x, for j=0 up to j=degree."""
+    degrees = np.arange(0, degree+1)
+    psi = np.power(x[:, np.newaxis], degrees)
+    return psi
